b069f277201a530314ce4b290a3e1ed06a0b0317
liamnicola <liamnicola@hotmail.com>
2022-04-06 14:37:32 +0100

    db connection

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-app.get('/songs', (req, res) => {
-    con.query('SELECT * FROM students', (error,results,fields) => {
-        if(error) {
-            res.status(500).json({error: error});
-        } else {
-            res.json(results);
-        }
-    });
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ae1'
+});
+
+con.connect(error => {
+	if(error) {
+		console.log(`Cannot connect to database: ${error}`);
+		process.exit(1);	
+	}
+	else {
+		console.log("Connected to database")
+    }
 });
